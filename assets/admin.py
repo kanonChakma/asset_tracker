0d4f64a20def2c13bb68ff592257b0d51725cbe5
kanon chakma <kanonchakma@pop-os.localdomain>
2023-06-03 15:52:33 +0600

    registered models

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("name", "location")
+
+
+@admin.register(models.Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+
+
+@admin.register(models.Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("company", "user", "is_staff", "devices")
+
+
+@admin.register(models.DeviceCheck)
+class DeviceCheckAdmin(admin.ModelAdmin):
+    list_display = (
+        "device",
+        "employee",
+        "log_type",
+        "checked_out_date",
+        "returned_date",
+        "condition_in",
+        "condition_out",
+    )
+
+
+@admin.register(models.DeviceAllocation)
+class DeviceAllocationAdmin(admin.ModelAdmin):
+    list_display = ("device", "employee", "start_date", "end_date")
